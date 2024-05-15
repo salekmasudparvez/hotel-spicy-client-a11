65aea31c17774f1268a10695b9b3a16b45219d31
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-05-15 13:40:48 +0600

    added price range filter

--- src/Components/RoomsList.jsx
+++ src/Components/RoomsList.jsx
@@ -2,44 +2,68 @@
 import { PropTypes } from 'prop-types';
 import { FaLuggageCart } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-const RoomsList = ({room}) => {
-    const { RoomImages, title, PricePerNight, Availability ,_id} = room
+const RoomsList = ({ room }) => {
+    const { RoomImages, title, PricePerNight, Availability, DiscountOffer, _id } = room
     const navigateList = useNavigate()
 
-    const handleNavigate=(id)=>{
+    const handleNavigate = (id) => {
         navigateList(`/rooms/${id}`)
-        
+
     }
     return (
         <>
-            <tr  className="border-b border-opacity-20 border-gray-700  text-center hover:bg-neutral-300 ">
-                <td onClick={()=>handleNavigate(_id)} className="p-3 ">
-                    <div className="avatar">
-                        <div className="w-24 mask mask-hexagon">
-                            <img src={RoomImages} />
+            {DiscountOffer ?
+                <tr className=" border-opacity-20 border-2 border-yellow-400  text-center hover:bg-neutral-300 ">
+                    <td onClick={() => handleNavigate(_id)} className="p-3 ">
+                        <div className="avatar">
+                            <div className="w-24 mask mask-hexagon">
+                                <img src={RoomImages} />
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td onClick={()=>handleNavigate(_id)} className="p-3">
-                    <p>{title}</p>
-                </td>
-                <td onClick={()=>handleNavigate(_id)} className="p-3 ">
-                    <p>{Availability?"Yes":"No"}</p>
-                    
-                </td>
-                <td onClick={()=>handleNavigate(_id)} className="p-3">
-                    <p className="text-gray-400">{PricePerNight}$</p>
-                </td>
-                <td className="">
-                <a onClick={()=>handleNavigate(_id)} className={`btn rounded-sm btn-md btn-outline ${!Availability && "btn-disabled"}`}>Book <FaLuggageCart /></a>
-                </td>
-            </tr>
+                    </td>
+                    <td onClick={() => handleNavigate(_id)} className="p-3">
+                        <p>{title}</p>
+                    </td>
+                    <td onClick={() => handleNavigate(_id)} className="p-3 ">
+                        <p>{Availability ? "Yes" : "No"}</p>
+
+                    </td>
+                    <td onClick={() => handleNavigate(_id)} className="p-3">
+                        <p className="text-gray-400"><span className='line-through'>${PricePerNight}</span> <span>${ PricePerNight-PricePerNight*(DiscountOffer/100)}</span></p>
+                    </td>
+                    <td className="">
+                        <a onClick={() => handleNavigate(_id)} className={`btn rounded-sm btn-md btn-outline ${!Availability && "btn-disabled"}`}>Book <FaLuggageCart /></a>
+                    </td>
+                </tr> :
+                <tr className="border-b border-opacity-20 border-gray-700  text-center hover:bg-neutral-300 ">
+                    <td onClick={() => handleNavigate(_id)} className="p-3 ">
+                        <div className="avatar">
+                            <div className="w-24 mask mask-hexagon">
+                                <img src={RoomImages} />
+                            </div>
+                        </div>
+                    </td>
+                    <td onClick={() => handleNavigate(_id)} className="p-3">
+                        <p>{title}</p>
+                    </td>
+                    <td onClick={() => handleNavigate(_id)} className="p-3 ">
+                        <p>{Availability ? "Yes" : "No"}</p>
+
+                    </td>
+                    <td onClick={() => handleNavigate(_id)} className="p-3">
+                        <p className="text-gray-400">{PricePerNight}$</p>
+                    </td>
+                    <td className="">
+                        <a onClick={() => handleNavigate(_id)} className={`btn rounded-sm btn-md btn-outline ${!Availability && "btn-disabled"}`}>Book <FaLuggageCart /></a>
+                    </td>
+                </tr>
+            }
         </>
     );
 };
 
-RoomsList.propTypes={
-    room:PropTypes.object
+RoomsList.propTypes = {
+    room: PropTypes.object
 }
 
 export default RoomsList;
--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,12 +1,13 @@
 
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import RoomCard from "./RoomCard";
 import { BsGrid3X3GapFill } from "react-icons/bs";
 import { FaArrowAltCircleUp, FaArrowCircleDown, FaBars, FaFilter } from "react-icons/fa";
 import RoomsList from "../../Components/RoomsList";
 import { useLoaderData } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet";
+import { useQuery } from "@tanstack/react-query";
+
 
 
 const Rooms = () => {
@@ -17,12 +18,37 @@ const Rooms = () => {
     const numberOfPages = Math.ceil(count / itemPerPage);
     const pages = [...Array(numberOfPages).keys()];
     const [currentPage, setCurrentPage] = useState(0);
-    const[filterPrice , SetFilterPrice]=useState(1);
+    const [filterPrice, SetFilterPrice] = useState(1);
+    const [rooms, setroom] = useState([]);
+    const [hprice, sethprice] = useState(1000);
+    const [lprice, setlprice] = useState(0)
 
+    const [isChange, setisChange] = useState(false)
+
+    //    console.log(lowPrice, highPrice);
     const handlePerPage = (e) => {
         const customItemPerPage = parseInt(e.target.value);
         setItemPerPage(customItemPerPage);
         setCurrentPage(0)
+    }
+    const handlePriceRange = (e) => {
+        e.preventDefault();
+        const highValue = e.target.high.value;
+        const lowValue = e.target.low.value;
+        sethprice(highValue);
+        setlprice(lowValue);
+        setisChange(true);
+
+        console.log(lowValue, typeof highValue);
+
+        //    fetch(`http://localhost:5000/roomsfilter?lprice=${lowValue}&hprice=${highValue}`)
+        //    .then(response=>{
+        //     response.json()
+        //    })
+        //    .then(data=>{
+        //     console.log(data);
+        //    })
+
     }
 
 
@@ -36,14 +62,80 @@ const Rooms = () => {
             setCurrentPage(currentPage + 1)
         }
     }
+    
+    // useEffect(()=>{
+       
+    //     if (isChange) {
+    //         fetch(`http://localhost:5000/rooms?page=${currentPage}&size=${itemPerPage}&sort=${filterPrice}&lprice=${lprice}$hprice=${hprice}`)
+    //         .then(res=>{
+    //             res.json()
+    //         })
+    //         .then(data=>{
+    //             console.log(data);
+    //         });
+           
+    //      } else {
+    //         console.log(isChange);
+    //          fetch(`http://localhost:5000/rooms?page=${currentPage}&size=${itemPerPage}&sort=${filterPrice}&lprice=${lprice}$hprice=${hprice}`)
+    //         .then(res=>{
+    //             res.json()
+    //         })
+    //         .then(data=>{
+    //             console.log(data);
+    //             setroom(data)
+    //         });
+    //      }
+        
+    // },[currentPage,filterPrice,hprice,lprice,itemPerPage,isChange])
 
+    // const { isPending, data: roomsdata } = useQuery({
+    //     queryKey: ['roomsdata', currentPage, itemPerPage,isChange, filterPrice],
+    //     queryFn: async () => {
+            
+    //         if (isChange) {
+    //            const res = await fetch(`http://localhost:5000/rooms?page=${currentPage}&size=${itemPerPage}&sort=${filterPrice}&lprice=${lprice}$hprice=${hprice}`);
+    //            console.log('sort by range');
+    //            return res.json()
+    //         } else {
+    //             const res = await fetch(`http://localhost:5000/rooms?page=${currentPage}&size=${itemPerPage}&sort=${filterPrice}`);
+    //             console.log('without range');
+    //             return res.json()
+
+    //         }
+
+    //     }
+    // })
     const { isPending, data: roomsdata } = useQuery({
-        queryKey: ['roomsdata',currentPage,itemPerPage,filterPrice],
+        queryKey: ['roomsdata', currentPage, itemPerPage, isChange, filterPrice, lprice, hprice],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/rooms?page=${currentPage}&size=${itemPerPage}&sort=${filterPrice}`)
-            return res.json()
+            let url = `http://localhost:5000/rooms?page=${currentPage}&size=${itemPerPage}&sort=${filterPrice}`;
+            
+            if (isChange) {
+                // If filtering by price range is requested, append the price range parameters to the URL
+                url += `&lprice=${lprice}&hprice=${hprice}`;
+            }
+    
+            try {
+                const res = await fetch(url);
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            } catch (error) {
+                console.error('Error fetching room data:', error);
+                throw error; // Rethrow the error to be caught by React Query
+            }
         }
-    })
+    });
+    
+    useEffect(() => {
+      if(roomsdata){
+        setroom(roomsdata)
+      }
+    }, [roomsdata])
+    
+
+    
     if (isPending) {
         return <div className="w-full flex justify-center items-center my-10">
             <span className="loading loading-spinner loading-lg text-accent"></span>
@@ -52,7 +144,7 @@ const Rooms = () => {
 
     return (
         <div className="md:mx-10 mx-2 space-y-4">
-              <Helmet>
+            <Helmet>
                 <meta charSet="utf-8" />
                 <title>Hotel Spicy || Rooms</title>
                 <link rel="canonical" href="http://mysite.com/example" />
@@ -63,15 +155,21 @@ const Rooms = () => {
             </div>
             <div className="border flex justify-between items-center border-neutral-300">
                 <div>
-                    <details className="dropdown rounded-sm bg-neutral-200 ">
+                    <details className="dropdown rounded-sm bg-neutral-200 md:flex hidden ">
                         <summary className="m-1 btn bg-transparent"><FaFilter />Price</summary>
                         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                            <li onClick={()=>SetFilterPrice(1)} className="border"><a>Price low to hight <FaArrowAltCircleUp /></a></li>
-                            <li onClick={()=>SetFilterPrice(-1)} className="border"><a>Price hight to low <FaArrowCircleDown /></a></li>
+                            <li onClick={() => SetFilterPrice(1)} className="border"><a>Price low to hight <FaArrowAltCircleUp /></a></li>
+                            <li onClick={() => SetFilterPrice(-1)} className="border"><a>Price hight to low <FaArrowCircleDown /></a></li>
                         </ul>
                     </details>
                 </div>
-                <div className="flex gap-2 justify-center items-center">
+                <form onSubmit={handlePriceRange} className="join justify-center border border-black items-center md:px-2">
+                    <h1 className="flex justify-center btn-sm md:btn-md  btn items-center md:gap-2 text-base font-semibold join-item"><FaFilter />Price Range:</h1>
+                    <input name="low" type="text" className="join-item w-12 p-1" required placeholder="Low" />
+                    <input name="high" type="text" className="join-item w-12 p-1" required placeholder="High" />
+                    <button type="submit" className="join-item btn btn-sm btn-outline">Filter</button>
+                </form>
+                <div className=" md:flex hidden gap-2 justify-center items-center">
                     <h1>View</h1>
                     <div className="join rounded-sm">
                         <a onClick={() => setLayout('grid')} className={`join-item btn ${layout === 'grid' ? "btn-active" : ""} `} ><BsGrid3X3GapFill /></a>
@@ -82,7 +180,7 @@ const Rooms = () => {
             </div>
             {layout === 'grid' ?
                 <div className="grid gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3 grid-cols-1" >
-                    {roomsdata?.map((room, idx) => <RoomCard room={room} idx={idx} key={idx}></RoomCard>)}
+                    {rooms?.map((room, idx) => <RoomCard room={room} idx={idx} key={idx}></RoomCard>)}
                 </div>
                 :
                 <div className="overflow-x-auto">
@@ -106,12 +204,12 @@ const Rooms = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {roomsdata?.map((room, idx) => <RoomsList key={idx} room={room}></RoomsList>)}
+                            {rooms?.map((room, idx) => <RoomsList key={idx} room={room}></RoomsList>)}
                         </tbody>
                     </table>
                 </div>
             }
-            <div className='flex py-10 gap-7 justify-center items-center'>
+            <div className='flex flex-col md:flex-row py-10 gap-7 justify-center items-center'>
                 <div className='flex gap-6'>
                     <a className="btn btn-sm btn-primary" onClick={handlePrevPage}>Prev.</a>
 

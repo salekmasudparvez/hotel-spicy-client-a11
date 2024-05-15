8ac35b16abad17e6de5fc60b98a8c8b9380dcb47
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-05-15 19:58:44 +0600

    added some style

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -33,14 +33,14 @@ const Home = () => {
             {/* You can open the modal using document.getElementById('ID').showModal() method */}
 
             <dialog id="my_modal_7" className="modal">
-                <div onClick={() => navigateNew('/rooms')} className="relative modal-box bg-no-repeat bg-cover bg-center min-h-[200px] bg-[url('https://i.postimg.cc/Y2TfwP8J/deluxe-queen-standard.jpg')]">
+                <div  className="relative modal-box bg-no-repeat bg-cover bg-center min-h-[200px] bg-[url('https://i.postimg.cc/Y2TfwP8J/deluxe-queen-standard.jpg')]">
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
                         <button className="btn bg-white btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
-                    <h3 className="font-bold text-white text-lg">Deluxe Queen Room</h3>
+                    <h3 onClick={() => navigateNew('/rooms')} className="font-bold text-white text-lg">Deluxe Queen Room</h3>
 
-                    <div className="bg-white absolute top-1/3 left-1/3 md:p-4 p2 rounded-sm -rotate-45"><h1 className="md:text-3xl text-xl font-bold text-yellow-500"> Discout 50%</h1></div>
+                    <div onClick={() => navigateNew('/rooms')} className="bg-white absolute top-1/3 left-1/3 md:p-4 p2 rounded-sm -rotate-45"><h1 className="md:text-3xl text-xl font-bold text-yellow-500"> Discout 50%</h1></div>
 
                 </div>
             </dialog>

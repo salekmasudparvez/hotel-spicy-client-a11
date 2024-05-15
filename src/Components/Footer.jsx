84df55dbe771c0aecbf76e2c48ea13e0f9cba486
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-05-15 20:19:46 +0600

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
         <footer className="footer p-10 bg-[#0A1121] text-white">
             <aside>
                   <img className='w-[150px] bg-gray-200 object-contain rounded-badge bg-opacity-60' src={Logo} alt="Trivago" />
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <p>Hotel Spicy Industries Ltd.<br />Providing reliable tech since 1992</p>
             </aside>
             <nav>
                 <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Booking</a>
+                <a className="link link-hover">Home</a>
                 <a className="link link-hover">Marketing</a>
                 <a className="link link-hover">Advertisement</a>
             </nav>
             <nav>
-                <h6 className="footer-title">Company</h6>
+                <h6 className="footer-title">Hotel Spicy Company</h6>
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
                 <a className="link link-hover">Jobs</a>

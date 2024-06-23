7550f443c38096ed35c52a515e022959420d6ec7
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-06-23 23:45:45 +0600

    update tanstack and added paysystem

--- src/Pages/Rooms/RoomDetails.jsx
+++ src/Pages/Rooms/RoomDetails.jsx
@@ -4,59 +4,77 @@ import useAuth from "../../Hook/useAuth";
 import axios from "axios";
 import ReviewPopup from "../Mybooking/ReviewPopup";
 import SpacificReviewCard from "../../Components/SpacificReviewCard";
-import { useEffect, useState } from "react";
-import Swal from 'sweetalert2'
+import SyncLoader from "react-spinners/ClipLoader";
+import { useQuery } from "@tanstack/react-query";
+//import Swal from 'sweetalert2'
 
 
 const RoomDetails = () => {
+
     const data = useLoaderData();
     const { user } = useAuth();
     const navigateList = useNavigate();
-    const [reviews, setReviews] = useState([]);
+    
   
     const { RoomImages, title, description, PricePerNight, Features, Availability, RoomSize, DiscountOffer, _id } = data;
 
-    const handleMyBooking =  e => {
-        e.preventDefault()
-        // if (user?.email === buyer?.email){return toast.error('Action not permitted!')}
+    // const handleMyBooking =  e => {
+    //     e.preventDefault()
+    //     // if (user?.email === buyer?.email){return toast.error('Action not permitted!')}
 
-        const clientEmail = e.target.email.value;
-        const name = e.target.name.value;
-        const bookingDate = e.target.date.value;
-        const bookTitle = title;
-        const image = RoomImages;
-        const currentDate = new Date()
+    //     const clientEmail = e.target.email.value;
+    //     const name = e.target.name.value;
+    //     const bookingDate = e.target.date.value;
+    //     const bookTitle = title;
+    //     const image = RoomImages;
+    //     const currentDate = new Date()
 
-        const bookingID = _id;
-        const Availability = false
-        const newBooking = {
-            clientEmail,
-            name,
-            bookingDate,
-            bookingID,
-            Availability,
-            bookTitle,
-            image,
-            currentDate,
+    //     const bookingID = _id;
+    //     const Availability = false
+    //     const newBooking = {
+    //         clientEmail,
+    //         name,
+    //         bookingDate,
+    //         bookingID,
+    //         Availability,
+    //         bookTitle,
+    //         image,
+    //         currentDate,
 
-        }
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "question",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, Book it!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-                 axios.post('https://hotel-server-kappa.vercel.app/mybooking', newBooking)
-                 axios.patch(`https://hotel-server-kappa.vercel.app/rooms/${_id}`, { Availability: false })
-                 document.getElementById('my_modal_1').showModal();
-                console.log(result.isConfirmed);
+    //     }
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "You won't be able to revert this!",
+    //         icon: "question",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Yes, Book it!"
+    //       }).then((result) => {
+    //         if (result.isConfirmed) {
+    //              axios.post('https://hotel-server-kappa.vercel.app/mybooking', newBooking)
+    //              axios.patch(`https://hotel-server-kappa.vercel.app/rooms/${_id}`, { Availability: false })
+    //              document.getElementById('my_modal_1').showModal();
+    //             console.log(result.isConfirmed);
                
+    //         }
+    //       });
+    // }
+    const handleMyBooking =(event)=>{
+        event.preventDefault()
+        const getRoomData = {
+            title,
+            customerName:user.displayName,
+            customerEmail:user.email,
+            amount:PricePerNight - PricePerNight * (DiscountOffer / 100),
+        }
+        axios.post('http://localhost:5000/create-payment',getRoomData)
+        .then(res=>{
+            const redirectUrl = res.data.paymentUrl
+            if(redirectUrl){
+                window.location.replace(redirectUrl)
             }
-          });
+        })
     }
     //reviews 
     const handleReview = (e) => {
@@ -86,14 +104,19 @@ const RoomDetails = () => {
                 // console.error('Error:', error);
             });
     }
-    useEffect(() => {
-        fetch('https://hotel-server-kappa.vercel.app/review')
-        .then(res=>res.json())
-        .then(data=>{
-          const spacificData = data.filter(dat=>dat.BookingIdForReview===_id);
-          setReviews(spacificData)
-        })
-     }, [_id])
+    const{isLoading:reviewLoading,data:reviews}=useQuery({
+        queryKey:['reviews',_id],
+        queryFn:async()=>{
+            const response = await axios.get(`http://localhost:5000/review?id=${_id}`)
+            const data = response.data
+            return data
+        }
+    })
+
+     if(!user || reviewLoading){
+        return ( <div className="min-h-screen w-full flex justify-center items-center"><SyncLoader height="100"
+            width="100" color="#36d7b7" /></div>)
+     }
     return (
         <section className=" text-gray-900">
             <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
@@ -107,7 +130,8 @@ const RoomDetails = () => {
                             {DiscountOffer ? <p className="font-bold text-gray-800 dark:text-gray-200"><span className='line-through'>${PricePerNight}</span> <span>${PricePerNight - PricePerNight * (DiscountOffer / 100)}</span></p>
                                 : <span className="font-bold text-gray-800 dark:text-gray-200">${PricePerNight}</span>} </div><p> <span className="font-medium">Room Size</span> :{RoomSize}</p> </div>
                         <div className="space-x-4">Fetures:{Features.map((feture, idx) => <span key={idx}>{feture},</span>)}</div>
-                        {user ? <form onSubmit={handleMyBooking}>
+                        {user ? 
+                        <form onSubmit={handleMyBooking}>
                             <label className="input input-bordered flex items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
                                 <input type="text" className="grow" name="name" placeholder="Username" value={user?.displayName || "unknown"} />
@@ -136,9 +160,9 @@ const RoomDetails = () => {
             {/* review of per card */}
             <div className="py-6">
                 <div><h1 className="text-4xl font-bold text-center py-5">Review of this room</h1></div>
-                {reviews.length>=1 ?
+                {reviews?.length>=1 ?
                 <div className="grid py-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-                {reviews.map((perRev,idx)=><SpacificReviewCard key={idx} perRev={perRev}></SpacificReviewCard>)}
+                {reviews?.map((perRev,idx)=><SpacificReviewCard key={idx} perRev={perRev}></SpacificReviewCard>)}
                 </div>:
                 <div className="h-32 flex justify-center items-center bg-white"><h1 className="text-4xl text-bold text-gray-400 text-center">No reviews Yet</h1></div>
                 }

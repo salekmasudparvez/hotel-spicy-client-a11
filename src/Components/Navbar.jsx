3e695263e4940922604dbecc4d2fc97a1556202e
salekmasudparvez <salekmasudparvez@gmail.com>
2024-09-15 17:51:14 +0600

    update :navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,24 @@ import Logo from '../assets/logo/logo-Final.png'
 import { FaBars } from 'react-icons/fa';
 import useAuth from '../Hook/useAuth';
 import useRole from '../Hook/useRole';
+import { Squash as Hamburger } from 'hamburger-react'
+import { useState } from 'react';
+import Navitem from './Navitem';
+
 const Navbar = () => {
-    const { user, LogOutUser,loading } = useAuth();
-    const [role,isLoading]=useRole();
+    const { user, LogOutUser, loading } = useAuth();
+    const [role, isLoading] = useRole();
+    const [isOpen, setOpen] = useState(false)
 
 
+
+    const navLinks = [
+        { name: 'Home', path: '/' },
+        { name: 'Rooms', path: '/rooms' },
+        { name: 'About Us', path: '/aboutus' },
+        { name: 'Contact', path: '/contact' }
+      ];
+      
     const links = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/rooms'>Rooms</Link></li>
@@ -16,21 +29,24 @@ const Navbar = () => {
     </>
     return (
         <div className='border-b w-full border-b-neutral-300'>
+
+          {  <ul className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box lg:hidden  absolute top-12 w-full text-sm transition-opacity  duration-1000 ${isOpen ? 'opacity-100' : 'opacity-0 -translate-y-96 '}`}>
+                {navLinks?.map((item,idx)=><Navitem name={item?.name} to={item?.path}></Navitem>)}
+            </ul>}
             <div className="navbar max-w-6xl mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn rounded-sm text-xl lg:hidden">
-                            <FaBars />
+                        <div className=" rounded-sm text-xl lg:hidden">
+                            <Hamburger toggled={isOpen} toggle={setOpen} />
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {links}
-                        </ul>
+
                     </div>
                     <Link to='/'><img className='w-[144px] h-[50px] object-contain' src={Logo} alt="Trivago" /></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {links}
+                    <ul className="flex gap-6 px-1">
+                    {navLinks?.map((item,idx)=><Navitem name={item?.name} to={item?.path}></Navitem>)}
+                    
                     </ul>
                 </div>
                 <div className="navbar-end gap-3">
@@ -42,9 +58,9 @@ const Navbar = () => {
                                 </div>
                                 <ul tabIndex={0} className="dropdown-content menu rounded  bg-white border  z-[1] w-52 p-2 shadow ">
                                     <li className='border'><Link className='rounded-none' to="/profile">Profile</Link></li>
-                                    {role==="host" && <li className='border'><Link className='rounded-none' to="/dashboard">Dashboard</Link></li>}
-                                    {role==="guest" && <li className='border'><Link className='rounded-none' to="/dashboard/guest">Dashboard</Link></li>}
-                                    {role==="admin" && <li className='border'><Link className='rounded-none' to="/dashboard/admin">Dashboard</Link></li>}
+                                    {role === "host" && <li className='border'><Link className='rounded-none' to="/dashboard">Dashboard</Link></li>}
+                                    {role === "guest" && <li className='border'><Link className='rounded-none' to="/dashboard/guest">Dashboard</Link></li>}
+                                    {role === "admin" && <li className='border'><Link className='rounded-none' to="/dashboard/admin">Dashboard</Link></li>}
                                 </ul>
                             </div>
                             <div>
---
+++ src/Components/Navitem.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import PropTypes from 'prop-types'
+
+const Navitem = ({name,to}) => {
+    return (
+        <NavLink
+            to={to}
+            className={({ isActive, isPending }) =>
+                isPending ? " font-semibold lg:border-b-2 border  lg:border-l-0 lg:border-b-[#5A95CE] lg:border-r-0 border-t-0 lg:bg-white bg-gray-200 lg:w-fit p-2 w-full  text-[#1a1a1a]" : isActive ? "font-semibold lg:border-b-2 lg:border-b-[#5A95CE] border  lg:border-l-0 lg:border-r-0 border-t-0 lg:bg-white bg-gray-200 lg:w-fit p-2 w-full text-[#000000] transition-colors duration-500" : "hover:text-[#515151] font-semibold lg:w-fit p-2 w-full lg:hover hover:bg-gray-200 transition-colors duration-500"
+            }
+        >
+            {name}
+        </NavLink>
+    );
+};
+Navitem.propTypes = {
+    name: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+}
+export default Navitem;
